32839615c7b354cbdbbf55ae2f4f7b4209daab98
Rojina Swain <127881366+rojina1624@users.noreply.github.com>
2023-09-23 19:50:16 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+
+    public class main {
+        public static void main(String[] args) {
+            BankAccount userAccount = new BankAccount(1000);
+            ATM atm = new ATM(userAccount);
+            atm.start();
+        }
+    } 
+
